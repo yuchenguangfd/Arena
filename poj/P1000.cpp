5ab90e91a715ad2a0a53a0a94e8336ed207ce3bd
yuchenguang <yuchenguangfd@gmail.com>
2015-05-05 21:14:01 +0800

    P1000:A+B Problem

---
+++ poj/P1000.cpp
@@ -0,0 +1,13 @@
+﻿/*
+ * Problem: A+B Problem
+ * http://poj.org/problem?id=1000
+ */
+ 
+#include <cstdio>
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	return 0;
+}
