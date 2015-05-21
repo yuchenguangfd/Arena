eb75a13089e7d437fa00b231acbc208f1a54fd98
yuchenguang <yuchenguangfd@gmail.com>
2015-05-22 00:13:55 +0800

    P3030:Nasty Hacks

---
+++ poj/P3030.cpp
@@ -0,0 +1,23 @@
+﻿/*
+ * Problem: Nasty Hacks
+ * http://poj.org/problem?id=3030
+ */
+
+#include <cstdio>
+
+int main() {
+    int numCase;
+    scanf("%d", &numCase);
+    while (numCase--) {
+        int r, e, c;
+        scanf("%d%d%d", &r, &e, &c);
+        if (r > e - c) {
+            printf("do not advertise\n");
+        } else if (r < e - c) {
+            printf("advertise\n");
+        } else {
+            printf("does not matter\n");
+        }
+    }
+    return 0;
+}
