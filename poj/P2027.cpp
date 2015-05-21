24f69d163f5d8f25193f7f23b2019369c74745b2
yuchenguang <yuchenguangfd@gmail.com>
2015-05-21 23:59:31 +0800

    P2027:No Brainer

---
+++ poj/P2027.cpp
@@ -0,0 +1,21 @@
+﻿/*
+ * Problem: No Brainer
+ * http://poj.org/problem?id=2027
+ */
+
+#include <cstdio>
+
+int main() {
+	int numCase;
+	scanf("%d", &numCase);
+	while(numCase--) {
+		int a, b;
+		scanf("%d %d",&a,&b);
+		if (a < b) {
+			printf("NO BRAINS\n");
+		} else {
+			printf("MMM BRAINS\n");
+		}
+	}
+	return 0;
+}
