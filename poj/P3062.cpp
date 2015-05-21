40d7fc1b9f51d6487794296f007eca3c6b542678
yuchenguang <yuchenguangfd@gmail.com>
2015-05-22 00:14:29 +0800

    P3062:Celebrity jeopardy

---
+++ poj/P3062.cpp
@@ -0,0 +1,12 @@
+﻿/*
+ * Problem: Celebrity jeopardy
+ * http://poj.org/problem?id=3062
+ */
+
+#include <cstdio>
+
+int main() {
+	char ch;
+	while ((ch = getchar()) != EOF) putchar(ch);
+	return 0;
+}
