330465a2ffb47ab49992bc3e885bc1a8d299b02a
yuchenguang <yuchenguangfd@gmail.com>
2015-06-20 07:41:04 +0800

    P1251:Jungle Roads

---
+++ poj/P1251.cpp
@@ -0,0 +1,102 @@
+/*
+ * Problem: Jungle Roads
+ * http://poj.org/problem?id=1251
+ */
+
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <cstdio>
+
+template <class T>
+class WeightedDirectedGraphAsAdjMatrix {
+public:
+    typedef T WeightType;
+    WeightedDirectedGraphAsAdjMatrix(int numVertex, WeightType initWeight = WeightType()) :
+        mNumVertex(numVertex), mWeights(numVertex * numVertex) {
+        std::fill(mWeights.begin(), mWeights.end(), initWeight);
+    }
+    int NumVertex() const { return mNumVertex; }
+    WeightType GetWeight(int u, int v) const { return mWeights[u * mNumVertex + v]; }
+    void AddEdge(int u, int v, WeightType w) { mWeights[u * mNumVertex + v] = w; }
+protected:
+    int mNumVertex;
+    std::vector<WeightType> mWeights;
+};
+
+template <class GraphType>
+class Prim {
+private:
+    typedef typename GraphType::WeightType WeightType;
+public:
+    Prim(const GraphType *graph) : mGraph(graph), mDists(graph->NumVertex()), mStates(graph->NumVertex()),
+        mPrevs(graph->NumVertex()), mTotalWeight(0) { }
+    void Compute() {
+        int source = 0;
+        for (int u = 0; u < mGraph->NumVertex(); ++u) {
+            mDists[u] = mGraph->GetWeight(source, u);
+            mStates[u] = false;
+            mPrevs[u] = source;
+        }
+        mDists[source] = 0;
+        mStates[source] = true;
+        mTotalWeight = 0;
+        for (int i = 1; i < mGraph->NumVertex(); ++i) {
+            WeightType min = std::numeric_limits<WeightType>::max();
+            int k;
+            for (int j = 0; j < mGraph->NumVertex(); ++j) {
+                if (!mStates[j] && min > mDists[j]) {
+                    min = mDists[j];
+                    k = j;
+                }
+            }
+            mStates[k] = true;
+            mDists[k] = min;
+            mTotalWeight += min;
+            for (int j = 0; j < mGraph->NumVertex(); ++j) {
+                if (!mStates[j] && mGraph->GetWeight(k, j) < mDists[j]) {
+                    mDists[j] = mGraph->GetWeight(k, j);
+                    mPrevs[j] = k;
+                }
+            }
+        }
+    }
+    WeightType TotalWeight() const {
+        return mTotalWeight;
+    }
+    const std::vector<int>& TreeAsPrevs() const {
+        return mPrevs;
+    }
+private:
+    const GraphType *mGraph;
+    std::vector<WeightType> mDists;
+    std::vector<bool> mStates;
+    std::vector<int> mPrevs;
+    WeightType mTotalWeight;
+};
+
+int main() {
+    while (true) {
+        int n;
+        scanf("%d", &n);
+        if (n == 0) break;
+        WeightedDirectedGraphAsAdjMatrix<int> graph(n, 0x7fffffff);
+        for (int i = 1; i < n; ++i) {
+            char str[16];
+            int k;
+            scanf("%s%d", str, &k);
+            int u = str[0] - 'A';
+            for (int j = 0; j < k; ++j) {
+                int v, w;
+                scanf("%s%d", str, &w);
+                v = str[0] - 'A';
+                graph.AddEdge(u, v, w);
+                graph.AddEdge(v, u, w);
+            }
+        }
+        Prim<WeightedDirectedGraphAsAdjMatrix<int> > prim(&graph);
+        prim.Compute();
+        printf("%d\n", prim.TotalWeight());
+    }
+    return 0;
+}
