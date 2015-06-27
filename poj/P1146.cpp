36a039e62a975aea66a3f9af4a71704fd29ddb95
yuchenguang <yuchenguangfd@gmail.com>
2015-06-28 07:23:43 +0800

    P1146:ID Codes

---
+++ poj/P1146.cpp
@@ -0,0 +1,28 @@
+﻿/*
+ * Problem: ID Codes
+ * http://poj.org/problem?id=1146
+ */
+
+#include <algorithm>
+#include <string>
+#include <cstdio>
+
+int main() {
+    char buff[64];
+    while (true) {
+        scanf("%s", buff);
+        std::string curr(buff);
+        if (curr == "#") break;
+        std::string next = curr;
+        bool flag = false;
+        do {
+            flag = std::next_permutation(next.begin(), next.end());
+        } while (flag && next == curr);
+        if (!flag) {
+            printf("No Successor\n");
+        } else {
+            printf("%s\n", next.c_str());
+        }
+    }
+    return 0;
+}
