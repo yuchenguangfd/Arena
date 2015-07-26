99b4ae1d3ea0a3a269cb6b177b45a7d158153200
yuchenguang <yuchenguangfd@gmail.com>
2015-07-27 00:06:07 +0800

    P3325:ICPC Score Totalizer Software

---
+++ poj/P3325.cpp
@@ -0,0 +1,26 @@
+﻿/*
+ * Problem: ICPC Score Totalizer Software
+ * http://poj.org/problem?id=3325
+ */
+
+#include <numeric>
+#include <algorithm>
+#include <vector>
+#include <cstdio>
+
+int main() {
+    while (true) {
+        int n;
+        scanf("%d", &n);
+        if (n == 0) break;
+        std::vector<int> score(n);
+        for (int i = 0; i < n; ++i) {
+            scanf("%d", &score[i]);
+        }
+        int avg = (std::accumulate(score.begin(), score.end(), 0)
+                - *(std::min_element(score.begin(), score.end()))
+                - *(std::max_element(score.begin(), score.end()))) / (n - 2);
+        printf("%d\n", avg);
+    }
+    return 0;
+}
