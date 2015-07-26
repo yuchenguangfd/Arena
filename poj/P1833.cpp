1ac88973c2d70117a2c8dcb3ff2b6eaac26ec8d6
yuchenguang <yuchenguangfd@gmail.com>
2015-07-27 00:57:16 +0800

    P1833:排列

---
+++ poj/P1833.cpp
@@ -0,0 +1,29 @@
+﻿/*
+ * Problem: 排列
+ * http://poj.org/problem?id=1833
+ */
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <cstdio>
+
+int main() {
+	int numCase;
+	scanf("%d",&numCase);
+	while (numCase--) {
+		int n, k;
+		scanf("%d %d",&n, &k);
+		std::vector<int> list(n);
+		for (int i = 0; i < n; ++i) {
+			scanf("%d", &list[i]);
+		}
+		for (int i = 0; i < k; ++i) {
+			std::next_permutation(list.begin(), list.end());
+		}
+		for (int i = 0; i < n; ++i) {
+		    std::cout << list[i] << ((i==n-1)?'\n':' ');
+		}
+	}
+	return 0;
+}
