2cac60bc0dbb3fd842b7490a3be2362e058d87d7
yuchenguang <yuchenguangfd@gmail.com>
2015-07-26 12:00:34 +0800

    P1922:Ride to School

---
+++ poj/P1922.cpp
@@ -0,0 +1,28 @@
+﻿/*
+ * Problem: Ride to School
+ * http://poj.org/problem?id=1922
+ */
+
+#include <algorithm>
+#include <cfloat>
+#include <cmath>
+#include <cstdio>
+
+int main() {
+    const double DISTANCE = 4.50;
+    while (true) {
+        int n;
+        scanf("%d", &n);
+        if (n == 0) break;
+        double minTime = DBL_MAX;
+        for (int i = 0; i < n; ++i) {
+            double v, t;
+            scanf("%lf %lf", &v, &t);
+            if (t >= 0) {
+                minTime = std::min(DISTANCE * 3600 / v + t, minTime);
+            }
+        }
+        printf("%.0lf\n", ceil(minTime));
+    }
+    return 0;
+}
