4a7898501e7840e39f30a06225eaff75899a91ef
yuchenguang <yuchenguangfd@gmail.com>
2015-08-05 12:24:53 +0800

    P3737:UmBasketella

---
+++ poj/P3737.cpp
@@ -0,0 +1,19 @@
+﻿/*
+ * Problem: UmBasketella
+ * http://poj.org/problem?id=3737
+ */
+
+#include <cmath>
+#include <cstdio>
+
+int main() {
+    const double PI = 3.141592653589793;
+    double S, R, H, V;
+    while (scanf("%lf", &S) != EOF) {
+        R = sqrt(S / PI) / 2;
+        H = sqrt(2 * S / PI);
+        V = PI * R * R * H / 3.0;
+        printf("%.2f\n%.2f\n%.2f\n",V, H, R);
+    }
+    return 0;
+}
