4e61ddcf30a776dc3c74d42fe21934d8db09cbfd
yuchenguang <yuchenguangfd@gmail.com>
2015-08-05 11:39:31 +0800

    P1658:Eva's Problem

---
+++ poj/P1658.cpp
@@ -0,0 +1,22 @@
+﻿/*
+ * Problem: Eva's Problem
+ * http://poj.org/problem?id=1658
+ */
+
+#include <cstdio>
+
+int main() {
+    int numCase;
+    scanf("%d", &numCase);
+    while (numCase--) {
+        int a1, a2, a3, a4, a5;
+        scanf("%d %d %d %d", &a1, &a2, &a3, &a4);
+        if (a1 + a3 == 2 * a2 && a2 + a4 == 2 * a3) {
+            a5 = a4 * 2 - a3;
+        } else {
+            a5 = a4 * a4 / a3;
+        }
+        printf("%d %d %d %d %d\n", a1, a2, a3, a4, a5);
+    }
+    return 0;
+}
