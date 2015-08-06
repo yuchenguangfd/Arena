f601ac43fd6331e703d972e589cdc7d16f884542
yuchenguang <yuchenguangfd@gmail.com>
2015-08-06 08:52:40 +0800

    P3994:Probability One

---
+++ poj/P3994.cpp
@@ -0,0 +1,17 @@
+﻿/*
+ * Problem: Probability One
+ * http://poj.org/problem?id=3994
+ */
+
+#include <cstdio>
+
+int main() {
+    int numCase = 0;
+    while (true) {
+        int n0;
+        scanf("%d", &n0);
+        if (n0 == 0) break;
+        printf("%d. %s %d\n", ++numCase, (n0 & 1) ? "odd" : "even", n0 / 2);
+    }
+    return 0;
+}
