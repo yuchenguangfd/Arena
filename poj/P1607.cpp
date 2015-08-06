cf502e8b29a60ac70b87247025178640f6a59e20
yuchenguang <yuchenguangfd@gmail.com>
2015-08-06 09:40:16 +0800

    P1607:Deck

---
+++ poj/P1607.cpp
@@ -0,0 +1,22 @@
+﻿/*
+ * Problem: Deck
+ * http://poj.org/problem?id=1607
+ */
+
+#include <cstdio>
+
+double harmonicNumber(int n) {
+	double sum = 0.0;
+	for(int i = 1; i <= n; ++i)
+		sum += 1.0/i;
+	return sum;
+}
+
+int main(){
+	printf("Cards  Overhang\n");
+	int n;
+	while (scanf("%d", &n) != EOF) {
+		printf("%5d%10.3f\n",n, harmonicNumber(n) / 2.0);
+	}
+	return 0;
+}
